50354e4155a0e7fd657103730ee78fe34ed45a46
Utkarsh Kumar <85729068+kumarutkarsh297@users.noreply.github.com>
2022-11-12 00:12:16 +0530

    01

--- livrary/urls.py
+++ livrary/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_header = "LIVRARY Admin"
-admin.site.site_title = "LIVRARY Admin Portal"
-admin.site.index_title = "Welcome to LIVRARY Admin Portal"
+admin.site.site_header = "LIVRARY | Admin Portal"
+# admin.site.site_title = "LIVRARY | Admin Portal"
+admin.site.index_title = "LIVRARY | Administration Portal"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
